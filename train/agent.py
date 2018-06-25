75ce4dd4b29f72b0de38fa46df085f46518a799e
Matt Turnshek <matthewturnshek@gmail.com>
2018-06-24 22:52:47 -0400

    continue testing basic convergence of tensorforce agents

--- train/agent.py
+++ train/agent.py
@@ -1,22 +1,56 @@
-from tensorforce.agents import DQNAgent
+import json
+
+from tensorforce.agents import DQNAgent, Agent
 from train_simulation_wrapper import SimulationWrapper
 
 environment = SimulationWrapper()
 
 # Network is an ordered list of layers
-network_spec = [dict(type='dense', size=256), dict(type='dense', size=128)]
+# network_spec = [dict(type='dense', size=256), dict(type='dense', size=128)]
+with open('tensorforce_configs/mlp2_network.json', 'r') as fp:
+    network_spec = json.load(fp=fp)
 
-agent = DQNAgent(
-    states=environment.states,
-    actions=environment.actions,
-    network=network_spec,
-    memory=dict(
-        type='replay',
-        include_next_states=True,
-        capacity=1000
-    ),
-    target_sync_frequency=10
-)
+with open('tensorforce_configs/ppo.json', 'r') as fp:
+    agent_config = json.load(fp=fp)
+
+# initial_epsilon = 0.6
+# final_epsilon = 0.0
+# anneal_timesteps = 100000
+agent = Agent.from_spec(
+        spec=agent_config,
+        kwargs=dict(
+            states=environment.states,
+            actions=environment.actions,
+            network=network_spec,
+        )
+    )
+# agent = DQNAgent(
+#     states=environment.states,
+#     actions=environment.actions,
+#     network=network_spec,
+#     memory=dict(
+#         type='replay',
+#         include_next_states=True,
+#         capacity=50000
+#     ),
+#     optimizer=dict(
+#         type='clipped_step',
+#         clipping_value=0.01,
+#         optimizer=dict(
+#             type='adam',
+#             learning_rate=1e-3
+#         )
+#     ),
+#     actions_exploration=dict(
+#         type='epsilon_anneal',
+#         initial_epsilon=initial_epsilon,
+#         final_epsilon=final_epsilon,
+#         timesteps=anneal_timesteps
+#     ),
+#     discount = 0.98,
+#     target_sync_frequency=100,
+#     double_q_model=True
+# )
 
 max_episodes = 1000
 max_timesteps = 2000
@@ -32,6 +66,10 @@ while True:
     episode_reward = 0.0
     while True:
         action_int = agent.act(states=state)
+        # print('\n\n')
+        # print('state', state)
+        # print('action', action_int)
+        # print('\n\n')
         state, reward, terminal = environment.execute(action_int)
         agent.observe(reward=reward, terminal=terminal)
 
@@ -42,7 +80,21 @@ while True:
             break
 
     episode += 1
-    print(episode_reward)
+    print('EPISODE', episode)
+    print('reward:', episode_reward)
+    print('num ship collisions: ', environment.episode_n_ship_collision)
+    print('num action passthroughs: ', environment.episode_n_action_passthrough)
+    print('num action overrides: ', environment.episode_n_action_override)
+    print('num nones: ', environment.episode_n_action_none)
+    print('num lefts: ', environment.episode_n_action_left)
+    print('num ups: ', environment.episode_n_action_up)
+    print('num rights: ', environment.episode_n_action_right)
+    print('num downs: ', environment.episode_n_action_down)
+    # total_timesteps = max_timesteps * episode
+    # current_epsilon = initial_epsilon - total_timesteps*(initial_epsilon - final_epsilon)/anneal_timesteps
+    # print('current epsilon: ', current_epsilon)
+    print('\n\n')
     episode_rewards.append(episode_reward)
+    #agent.save_model('saved_models/dqn_agent')
     if episode == max_episodes:
         break
--- train/train_simulation_wrapper.py
+++ train/train_simulation_wrapper.py
@@ -20,30 +20,47 @@ class SimulationWrapper:
         """
 
         # Tensorforce environment specs
-        self.states = dict(shape=(206,), type='float')
+        #self.states = dict(shape=(206,), type='float')
+        self.states = dict(shape=(5), type='float')
         self.actions = dict(type='int', num_actions=5)
 
+        self.reset_debug_vars()
+
+    def reset_debug_vars(self):
+        self.episode_n_ship_collision = 0
+        self.episode_n_action_passthrough = 0
+        self.episode_n_action_override = 0
+        self.episode_n_action_none = 0
+        self.episode_n_action_left = 0
+        self.episode_n_action_up = 0
+        self.episode_n_action_right = 0
+        self.episode_n_action_down = 0
+
     def get_state(self):
-        state_buffer = self.connection.get_state_buffer()
-        action =  self.action_gen.next_action()
-        return np.concatenate((state_buffer, action))
+        # state_buffer = self.connection.get_state_buffer()
+        action = self.action_gen.next_action()
+        return action
+        # return np.concatenate((state_buffer, action))
 
     def reset(self):
+        self.reset_debug_vars()
         self.connection.publish_action(self.action_gen.none)
         return self.get_state()
 
     def is_ship_colliding(self, state):
         # if this value in the state is 1.0, that means the ship is colliding
         # with an asteroid.
-        return (state[197] == 1.0)
+        return False
+        # return (state[197] == 1.0)
 
     def is_human_action_passed_through(self, state, action):
-        human_action = state[201:206]
-        return np.array_equal(action, human_action)
+        # human_action = state[201:206]
+        # return np.array_equal(action, human_action)
+        return np.array_equal(state, action)
 
-    def parse_reward(self, state, action):
-        ship_collision_reward = -5.0
-        human_action_passthrough_reward = 1.0
+    def parse_reward_and_log(self, state, action):
+        ship_collision_reward = 0.0 # just trying this to see if it learns or what
+        human_action_passthrough_reward = 0.0
         other_action_reward = 0.0
 
         # if there's a spaceship collision, reward is negative.
@@ -56,29 +73,39 @@ class SimulationWrapper:
         reward = 0.0
         if ship_collision:
             reward += ship_collision_reward
+            self.episode_n_ship_collision += 1
         if human_action_passthrough:
             reward += human_action_passthrough_reward
+            self.episode_n_action_passthrough += 1
         else:
             reward += other_action_reward
+            self.episode_n_action_override += 1
 
+        if np.array_equal(action, self.action_gen.left):
+            return 1.0
         return reward
 
-    def int_to_action_one_hot(self, action_int):
+    def int_to_action_one_hot_and_log(self, action_int):
         if action_int == 0:
+            self.episode_n_action_none += 1
             return self.action_gen.none
         elif action_int == 1:
+            self.episode_n_action_left += 1
             return self.action_gen.left
         elif action_int == 2:
+            self.episode_n_action_up += 1
             return self.action_gen.up
         elif action_int == 3:
+            self.episode_n_action_right += 1
             return self.action_gen.right
         elif action_int == 4:
+            self.episode_n_action_down += 1
             return self.action_gen.down
 
     def execute(self, action_int):
-        action = self.int_to_action_one_hot(action_int)
+        action = self.int_to_action_one_hot_and_log(action_int)
         self.connection.publish_action(action)
         state = self.get_state()
-        reward = self.parse_reward(state, action)
+        reward = self.parse_reward_and_log(state, action)
         terminal = False
         return (state, reward, terminal)
